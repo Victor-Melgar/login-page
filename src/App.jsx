29e224f384ff9ad44dca252a43f48735fd95aade
Victor-Melgar <gabrielmelgar.digital@gmail.com>
2024-10-22 16:36:44 -0300

    login-page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+import './App.css'
+
+function App() {
+
+  return (
+  
+    <div>
+      <h1 className="text-3xl font-bold underline bg-blue-600">Hello Word!</h1>
+    </div>
+
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/App.jsx",
+    "./src/components/Login/Login.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
