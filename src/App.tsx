59a228fc8a390ac4a4e4a61c571eff7a7c710a97
Victor-Melgar <gabrielmelgar.digital@gmail.com>
2024-10-31 10:30:48 -0400

    loja-page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+<link href="./output.css" rel="stylesheet"></link>
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center text-blue-600">Hello word!</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/login/login.tsx
@@ -0,0 +1,24 @@
+// import { useState } from "react";
+
+<link href="./output.css" rel="stylesheet"></link>
+
+const login = () => {
+
+  return (
+
+    // const [username, setUsername] = useState("");
+    //     const [password, setPassword] = useState("");
+
+    //     const handleSubmit = (event) => {
+    //         event.preventDefault();
+
+    //         alert("Eviando os dados:" + username + " - " + password);
+    //     }
+
+    <> 
+    
+    </>
+  )
+}
+
+export default login
